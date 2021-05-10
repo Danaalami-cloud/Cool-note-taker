a0c9ce1f11ee9909dda21c94c898649ed8d6f3cf
Dana Alami <dana.alami18@gmail.com>
2021-05-10 18:19:01 +0300

    fixed contents in public folder and routes folder

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,4 +1,11 @@
-const existingNotes = require('../data/existingnotes');
-const newNotes = require('../data/newnotes');
+const existingNotes = require('../data/existingNotes');
+const newNotes = require('../data/newNotes');
+const fs = require('fs');
 
 module.exports = (app) => {
+    app.get('/api/existing', (req, res) => res.json(existingNotes));
+    app.get('/api/newnotes', (req, res) => res.json(newNotes));
+
+    app.post('/api/newNotes', (req, res) => {
+        if (newNotes.length)
+    }
